d9f1027623354b93c6953477b6f2c9281793b299
Krztvl <c.penatroncoso@gmail.com>
2020-08-06 00:55:45 -0400

    Navbar basico funcionando, sin estilos

--- src/components/Header.svelte
+++ src/components/Header.svelte
@@ -6,66 +6,73 @@
   img {
     height: 90px;
   }
+  .text-transparent {
+    color: transparent;
+  }
 </style>
 
-<header>
-  <div class="row pt-2 px-2">
-    <div class="col text-md-center">
-      <a href="#">
-        <img src="images/logo-bellabike.png" alt="" />
-      </a>
-    </div>
+<!-- <header> -->
+<div class="row pt-2 px-2">
+  <div class="col text-md-center">
+    <a href="#">
+      <img src="images/logo-bellabike.png" alt="" />
+    </a>
   </div>
-  <div class="row">
-    <div class="col">
-      <nav class="navbar navbar-expand-lg navbar-light bg-light">
-        <div />
-        <button
-          class="navbar-toggler"
-          type="button"
-          data-toggle="collapse"
-          data-target="#navbarNavDropdown"
-          aria-controls="navbarNavDropdown"
-          aria-expanded="false"
-          aria-label="Toggle navigation">
-          <span class="navbar-toggler-icon" />
-        </button>
-        <div class="collapse navbar-collapse" id="navbarNavDropdown">
-          <ul class="navbar-nav">
-            <li class="nav-item active">
-              <a class="nav-link" href="#">
-                Home
-                <span class="sr-only">(current)</span>
-              </a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="#">Features</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="#">Pricing</a>
-            </li>
-            <li class="nav-item dropdown">
-              <a
-                class="nav-link dropdown-toggle"
-                href="#"
-                id="navbarDropdownMenuLink"
-                role="button"
-                data-toggle="dropdown"
-                aria-haspopup="true"
-                aria-expanded="false">
-                Dropdown link
-              </a>
-              <div
-                class="dropdown-menu"
-                aria-labelledby="navbarDropdownMenuLink">
-                <a class="dropdown-item" href="#">Action</a>
-                <a class="dropdown-item" href="#">Another action</a>
-                <a class="dropdown-item" href="#">Something else here</a>
-              </div>
-            </li>
-          </ul>
+</div>
+<nav class="navbar navbar-expand-lg navbar-light bg-light sticky-top ">
+  <a class="navbar-brand" href="#" />
+  <button
+    class="navbar-toggler"
+    type="button"
+    data-toggle="collapse"
+    data-target="#navbarSupportedContent"
+    aria-controls="navbarSupportedContent"
+    aria-expanded="false"
+    aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon" />
+  </button>
+
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="navbar-nav mx-auto">
+      <li class="nav-item">
+        <a
+          rel="prefecth"
+          class={segment === undefined ? 'nav-link active' : 'nav-link'}
+          href="/">
+          Home
+          <span class="sr-only">(current)</span>
+        </a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Arriendos</a>
+      </li>
+      <li class="nav-item dropdown">
+        <a
+          class="nav-link dropdown-toggle"
+          href="#"
+          id="navbarDropdown"
+          role="button"
+          data-toggle="dropdown"
+          aria-haspopup="true"
+          aria-expanded="false">
+          Tours
+        </a>
+        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+          <a class="dropdown-item" href="#">Santiago Histórico</a>
+          <a class="dropdown-item" href="#">Santiago Moderno</a>
+          <a class="dropdown-item" href="#">Cerro San Cristobal</a>
         </div>
-      </nav>
-    </div>
+      </li>
+      <li class="nav-item">
+        <a
+          rel="prefecth"
+          class={segment === 'nosotros' ? 'nav-link active' : 'nav-link'}
+          href="nosotros">
+          Nosotros
+        </a>
+      </li>
+
+    </ul>
   </div>
-</header>
+</nav>
+<!-- </header> -->
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,36 +1,17 @@
 <style>
-  h1,
-  figure,
-  p {
-    text-align: center;
-    margin: 0 auto;
+  .carousel-inner img {
+    max-height: 100vh;
+    object-fit: cover;
+    filter: grayscale(60%);
   }
-
-  h1 {
-    font-size: 2.8em;
-    text-transform: uppercase;
-    font-weight: 700;
-    margin: 0 0 0.5em 0;
-  }
-
-  figure {
-    margin: 0 0 1em 0;
-  }
-
-  img {
-    width: 100%;
-    max-width: 400px;
-    margin: 0 0 1em 0;
-  }
-
-  p {
-    margin: 1em auto;
-  }
-
-  @media (min-width: 480px) {
-    h1 {
-      font-size: 4em;
-    }
+  #carousel .overlay {
+    position: absolute;
+    top: 0;
+    bottom: 0;
+    left: 0;
+    right: 0;
+    background-color: rgba(0, 0, 0, 0.5);
+    color: white;
   }
 </style>
 
@@ -38,15 +19,67 @@
   <title>BellaBike Tours and Bike Rental</title>
 </svelte:head>
 
-<h1>Great success!</h1>
-
-<figure>
-  <img alt="Success Kid" src="successkid.jpg" />
-  <figcaption>Have fun with Sapper!</figcaption>
-</figure>
-
+<div id="carousel" class="carousel slide" data-ride="carousel">
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img
+        class="d-block w-100"
+        src="images/fondo.jpg"
+        alt="images/fondo.jpg" />
+    </div>
+  </div>
+  <div class="overlay carousel-caption">
+    <div class="overlay-container h-100">
+      <div class="row align-items-center justify-content-center h-100">
+        <div class="col-md-6">
+          <h1>Disfruta de Santiago en bicicleta</h1>
+          <p class="d-none d-md-block">
+            ¿De visita en la ciudad? Vives muy lejos como para llegar en
+            bicicleta al centro. Entonces Bellabike es lo que necesitas. Los
+            parques y ciclovías de la ciudad te esperan.
+          </p>
+          <a href="#" class="btn btn-outline-light">Quiero hacer tour</a>
+          <button
+            type="buttom"
+            class="btn btn-platzi"
+            data-toggle="modal"
+            data-target="#modalCompra">
+            Quiero arrendar una bici
+          </button>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
 <p>
-  <strong>
-    Try editing this file (src/routes/index.svelte) to test live reloading.
-  </strong>
+  Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nihil pariatur
+  ratione impedit aut assumenda ut accusamus facilis architecto vel obcaecati
+  fuga magnam, esse perferendis, tenetur repellat nemo voluptates magni totam
+  saepe? Alias corporis fugiat culpa suscipit praesentium aut libero voluptates!
+  Repellendus voluptas non nam eveniet perspiciatis earum quasi, tempora facere?
+  Perferendis consequuntur expedita culpa unde a blanditiis consectetur aliquam
+  maxime quidem consequatur voluptatem deserunt nostrum placeat quis vero odit
+  dolor delectus et, facilis rerum iusto numquam cumque. Esse, perspiciatis? Non
+  error, perferendis perspiciatis rerum ea sit tempora harum laboriosam! Nobis,
+  nostrum? Aliquam velit eius accusantium saepe provident! Voluptate, dolores
+  voluptas quo voluptates distinctio nesciunt quidem aliquid magni repudiandae
+  beatae atque! Fugiat, dolorem quisquam natus soluta hic deserunt ab illum at
+  excepturi aut itaque. A sed illum itaque sapiente! Expedita quaerat, vel
+  deserunt tenetur reprehenderit dolore nihil maxime corrupti aliquid ratione
+  tempore distinctio odit minima rerum quam at exercitationem reiciendis
+  veritatis soluta ipsum earum rem nisi culpa? Quasi fuga, nobis iure aliquam
+  nihil repellat minus, mollitia atque quibusdam quis ratione harum ea nostrum
+  reprehenderit ullam perferendis libero commodi? Perspiciatis laborum quae,
+  hic, velit quaerat facere blanditiis nostrum adipisci, officiis minima aliquam
+  dignissimos. Excepturi molestias asperiores nulla officiis vel sed ducimus
+  fuga repudiandae quibusdam, veritatis nemo deleniti consequuntur velit odio
+  sunt labore mollitia saepe error sequi, beatae qui aliquam ea quas? Magnam,
+  voluptate dolore. Deserunt neque qui, veritatis totam debitis quas, cupiditate
+  quidem iusto libero facilis aliquid! Praesentium eum voluptates hic quia
+  cumque rerum vel ipsam repellat dicta aspernatur vero distinctio, eveniet
+  perspiciatis fugiat maxime alias totam tenetur inventore ut laboriosam sequi!
+  Rerum nemo cumque consectetur soluta distinctio illum dolores sunt minima
+  nisi, officia saepe alias mollitia et, eveniet quis nesciunt minus ratione
+  eaque corporis est. Facere tempore quas earum reprehenderit labore aperiam
+  officiis magni fugit, itaque obcaecati debitis aut mollitia minima.
 </p>
